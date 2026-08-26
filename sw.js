02c197e1b4da122f3bd3ea876784dfaadcc197dc
stratigos79 <general1@otenet.gr>
2026-08-26 13:30:18 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C="sp30s4";const A=["./","./index.html","./style.css","./game.js","./manifest.json"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
